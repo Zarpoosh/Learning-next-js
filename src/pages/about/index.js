e5cd6d0a5ca9b232a91623a8929a57e8fcac47f8
Zarpoosh <mzar67839@gmail.com>
2024-05-27 01:27:14 +0330

    add font & image

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,13 +1,20 @@
-function AboutPage(){
-    return (
-      <div>
-        <h2 className="text-4xl font-bold m-3">
-          about page :))) minoo zarpoosh
-        </h2>
-      </div>
-    );
+import { Lora, Rubik } from "next/font/google";
+const rubik = Lora({ subsets: ["latin"] });
+import Image from "next/image";
+import Logo from "../"
+
+function AboutPage() {
+  return (
+    <div>
+      <h2 className={`text-4xl font-bold m-3 ${rubik.className}`}>
+        about page :))) minoo zarpoosh
+      </h2>
+      <Image
+        src={Logo}
+        alt="minicode"
+      />
+    </div>
+  );
 }
 
-
-
-export default AboutPage
+export default AboutPage;
